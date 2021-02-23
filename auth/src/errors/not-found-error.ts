6419c6d74a246331aa40507a77ecd127bf01fba4
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-02-24 03:25:30 +0530

    add Error and async error handling

---
+++ auth/src/errors/not-found-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from './custom-error'; 
+
+export class NotFoundError extends CustomError {
+  statusCode = 500;
+
+  constructor() {
+    super('Route not found');
+
+    // Only because we are extending a build in class
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      { message: 'Route not found' }
+    ]
+  }
+}
