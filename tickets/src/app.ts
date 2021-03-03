7b01f9ff2e2f08239b09d56ebfd0e6a3f3a76e2b
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-03-03 15:58:04 +0530

    ticketing & common service added

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,12 +3,6 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-// import { errorHandler } from './middlewares/error-handler';
-// import { NotFoundError } from './errors/not-found-error';
 import { errorHandler, NotFoundError } from '@jitusehrawattickets/common'
 
 const app = express();
@@ -22,11 +16,6 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.get('*', async (req, res) => {
   throw new NotFoundError();
