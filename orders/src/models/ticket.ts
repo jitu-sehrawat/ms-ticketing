7abae90f9c4b56b8d49a77507dffce646f0d5c42
Jitender Sehrawat <jitu.sehrawat@gmail.com>
2021-03-08 01:43:02 +0530

    Create order route done

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import { Order, OrderStatus } from './order';
+
+// Cannot write the same or suer thr Ticket service ticketschema as
+// order service requires only 2-3 relevent prop of ticket to create a order.
+// So cannot include in Common service. 
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+  }
+});
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+ticketSchema.methods.isReserved = async function() {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitngPayment,
+        OrderStatus.Complete
+      ]
+    }
+  });
+
+  return !!existingOrder;
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,56 @@
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@jitusehrawattickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    
+    // Find the ticket user id trying to order in the DB
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate the an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+    
+    // Build the order and save it to the DB
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket
+    });
+    await order.save();
+
+    // Publish an event saying that an order was created
+    
+    res.status(201).send(order);
+  }
+);
 
 export { router as newOrderRouter };
